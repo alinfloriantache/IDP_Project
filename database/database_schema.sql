bf79065e312fcff65136b04904f613af5f840baa
alin-f-tache <alin.f.tache@gmail.com>
2020-04-03 19:35:43 +0300

    Update schema

--- database/database_schema.sql
+++ database/database_schema.sql
@@ -2,8 +2,8 @@ CREATE DATABASE music_app_db;
 USE music_app_db;
 
 CREATE TABLE songs(
-	id bigint(11) NOT NULL AUTO_INCREMENT COMMENT "Primary key",
-	title varchar(30) COMMENT "Title of the song",
-	artists varchar(50) COMMENT "Artists of the song",
+	id bigint(11) NOT NULL AUTO_INCREMENT,
+	title varchar(30),
+	artists varchar(50),
 	PRIMARY KEY (id)
 );
