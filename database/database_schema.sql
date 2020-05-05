5144eff18852b075f9e6120c42b86e2877cf1ec5
alin-f-tache <alin.f.tache@gmail.com>
2020-05-05 16:01:09 +0300

    remove pyc

--- database/database_schema.sql
+++ database/database_schema.sql
@@ -3,7 +3,30 @@ USE music_app_db;
 
 CREATE TABLE songs(
 	id bigint(11) NOT NULL AUTO_INCREMENT,
-	title varchar(30),
-	artists varchar(50),
+	name varchar(100),
 	PRIMARY KEY (id)
+);
+
+CREATE TABLE users(
+	id bigint(11) NOT NULL AUTO_INCREMENT,
+	username varchar(30),
+	password varchar(30),
+	uploader tinyint(1),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE playlists(
+	id bigint(11) NOT NULL AUTO_INCREMENT,
+	userid bigint(11),
+	title varchar(30),
+	PRIMARY KEY (id),
+    FOREIGN KEY (userid) REFERENCES users(id)
+);
+
+CREATE TABLE playlistsongs(
+	id bigint(11) NOT NULL AUTO_INCREMENT,
+	playlistid bigint(11),
+	songid bigint(11),
+    FOREIGN KEY (playlistid) REFERENCES playlists(id)
+    FOREIGN KEY (songid) REFERENCES songs(id)
 );
