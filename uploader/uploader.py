7907691c5892dea35a7d484b00528840e92563d6
alin-f-tache <alin.f.tache@gmail.com>
2020-05-05 17:03:56 +0300

    db test

--- uploader/uploader.py
+++ uploader/uploader.py
@@ -13,13 +13,27 @@ class Uploader(flask.views.MethodView):
 	@utils.login_required
 	def get(self):
 		songs = os.listdir("static/music")
-		return flask.render_template("uploader.html", songs=songs)
+
+		conn = utils.get_db_connection()
+		cursor = conn.cursor()
+		query = "SELECT * FROM songs".format(f.filename)
+		cursor.execute(query)
+		result = cursor.fetchall()
+		conn.close()
+
+		return flask.render_template("uploader.html", songs=songs, entries=flask.jsonify(result))
 
 	@utils.login_required
 	def post(self):
 		f = flask.request.files["file"]
 		if is_mp3_file(f.filename):
 			f.save(os.path.join(flask.current_app.config["UPLOAD_FOLDER"], secure_filename(f.filename)))
+			conn = utils.get_db_connection()
+			cursor = conn.cursor()
+			query = "INSERT INTO songs(name) VALUES ({0})".format(f.filename)
+			cursor.execute(query)
+			conn.close()
+
 			flask.flash("File uploaded succesfully!")
 		else:
 			flask.flash("Format not supported!")
